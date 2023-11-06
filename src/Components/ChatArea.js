7d6f0ff26d87356f28dbed00b7511982d450d527
User <Thirukkumaran-S>
2023-11-06 20:07:50 +0530

    front-end message others and message self UI completed

--- src/Components/ChatArea.js
+++ src/Components/ChatArea.js
@@ -1,31 +1,34 @@
-import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import { IconButton } from '@mui/material';
-import SendIcon from '@mui/icons-material/Send';
-
-function ChatArea({props}) {
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+import MessageOthers from "./MessageOthers";
+import MessageSelf from "./MessageSelf";
+function ChatArea({ props }) {
   return (
-    <div className='chatarea-container'>
-        <div className='chatarea-header'>
-            <p className='con-icon'>{props.name[0]}</p>
-            <div className='header-text'>
-                <p className='con-title'>{props.name}</p>
-                <p className='con-timeStamp'>{props.timeStamp}</p>
-            </div>
-            <IconButton>
-                <DeleteIcon/>
-            </IconButton>
-
-        </div>    
-        <div className='messages-container'>message area</div>    
-        <div className='text-input-area'>
-            <input placeholder='Type a Message' className='search-box'/>
-            <IconButton>
-                <SendIcon/>
-            </IconButton>
-        </div>    
+    <div className="chatarea-container">
+      <div className="chatarea-header">
+        <p className="con-icon">{props.name[0]}</p>
+        <div className="header-text">
+          <p className="con-title">{props.name}</p>
+          <p className="con-timeStamp">{props.timeStamp}</p>
+        </div>
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </div>
+      <div className="messages-container">
+        <MessageOthers />
+        <MessageSelf />
+      </div>
+      <div className="text-input-area">
+        <input placeholder="Type a Message" className="search-box" />
+        <IconButton>
+          <SendIcon />
+        </IconButton>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChatArea
+export default ChatArea;
--- src/Components/MessageSelf.js
+++ src/Components/MessageSelf.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 function MessageSelf() {
+  var props2={name:"You",message:"This is a sample message"}
   return (
-    <div className='self-message-container'>MessageSelf</div>
+    <div className='self-message-container'>
+      <div className='messageBox'><p>{props2.message}</p>
+        <p className='self-timeStamp'>12.00am</p>
+      </div>
+    </div>
   )
 }
 
